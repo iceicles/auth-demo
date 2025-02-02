0311002fa8e2aa0f74a06bbed090c8fee57c4632
oshiogifted <oshiogifted2011@gmail.com>
2025-02-01 23:06:22 -0500

    changes to jwtsignature interface

--- backend/interfaces/JWTSig.ts
+++ backend/interfaces/JWTSig.ts
@@ -1,8 +1,10 @@
+// interface used in creating user property for jwt signature
 export interface ITokenUser {
   userId: string;
   name: string;
 }
 
+// interface used in creating jwt signature
 export interface IJWTSignature {
   user: ITokenUser,
   refreshToken?: string; // refresh token created from crypto - used to sign refreshToken only
