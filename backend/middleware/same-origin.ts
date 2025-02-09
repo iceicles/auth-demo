2f1f463f4555a4d34a9ba1e298c1892a8db2291f
oshiogifted <oshiogifted2011@gmail.com>
2025-02-09 02:10:16 -0500

    implemented middleware to prevent csrf

---
+++ backend/middleware/same-origin.ts
@@ -0,0 +1,22 @@
+import { StatusCodes } from "http-status-codes";
+
+const allowedOrigins = process.env.CLIENT_URL || '';
+
+// same origin middleware to prevent CSRF
+export const sameOriginMiddleware = (req: any, res: any, next: any) => {
+  const referer = req.get('Referer');
+  const origin = req.get('Origin'); // sometimes Origin header can be used as well
+
+
+  // extract the origin of the Referer (if available)
+  const refererOrigin = referer ? new URL(referer).origin : '';
+  const originHeader = origin ? new URL(origin).origin : '';
+
+  // allow requests from trusted domain
+  if (allowedOrigins.includes(refererOrigin) || allowedOrigins.includes(originHeader)) {
+    return next(); // request is from a trusted origin
+  }
+
+  // block the request if it comes from an untrusted domain
+  return res.status(StatusCodes.FORBIDDEN).json({ message: 'Forbidden: Invalid origin' });
+};
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -2,11 +2,12 @@ import express from 'express';
 
 import { authenticateUser } from '../middleware/authentication';
 import { showCurrentUser } from '../controllers/user';
+import { sameOriginMiddleware } from '../middleware/same-origin';
 
 const router = express.Router();
 
 // authMiddleware is executed first before dashboard is called
-router.route('/showMe').get(authenticateUser, showCurrentUser)
+router.route('/showMe').get(sameOriginMiddleware, authenticateUser, showCurrentUser)
 
 
 export default router;
