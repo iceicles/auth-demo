b53202e016889cefcbfef6c6acc67413694ac857
oshiogifted <oshiogifted2011@gmail.com>
2025-02-09 02:50:25 -0500

    removed default fonts

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import { AuthProvider } from '@/providers/AuthProvider';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'Auth Demo',
@@ -28,11 +16,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <AuthProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+        <body>{children}</body>
       </AuthProvider>
     </html>
   );
